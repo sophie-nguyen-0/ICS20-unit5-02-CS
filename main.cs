b4a11d80d009385134592089450e1a700dbbf8a5
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-17 19:04:01 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program {
+    public static void Main (string[] args) {
+        int dice;
+
+        System.Random random = new System.Random();
+
+        Console.Write("if you want positive dice, type 1, if you want a negative, type 2: ");
+        dice = Convert.ToInt32(Console.ReadLine());
+
+        if (dice == 1)
+        {
+            int num;
+
+            num = random.Next(6);
+
+             Console.WriteLine("your number is"+num+".");
+        }
+        else 
+        {
+            int num;
+
+            num = random.Next(-6, -1);
+
+            Console.WriteLine("your number is"+num+".");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
+}
