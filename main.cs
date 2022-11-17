6bb52ea52d5b07c9408607a3bd6817ebe77b21d9
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-17 19:07:38 +0000

    stuff

--- main.cs
+++ main.cs
@@ -1,12 +1,14 @@
 using System;
 
-class Program {
-    public static void Main (string[] args) {
+class Program
+{
+    public static void Main(string[] args)
+    {
         int dice;
 
         System.Random random = new System.Random();
 
-        Console.Write("if you want positive dice, type 1, if you want a negative, type 2: ");
+        Console.Write("if you want positive dice, type 1: ");
         dice = Convert.ToInt32(Console.ReadLine());
 
         if (dice == 1)
@@ -23,7 +25,7 @@ class Program {
 
             num = random.Next(-6, -1);
 
-            Console.WriteLine("your number is"+num+".");
+             Console.WriteLine("your number is"+num+".");
         }
 
         Console.WriteLine("\nDone.");
